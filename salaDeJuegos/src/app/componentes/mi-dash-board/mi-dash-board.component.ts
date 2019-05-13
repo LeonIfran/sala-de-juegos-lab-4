92e94a66546a101703a6d622928521108aa38dfe
LeonIfran <leonsifran@gmail.com>
2019-05-13 02:19:13 -0300

    arreglado el dashboard para vista movil

--- salaDeJuegos/src/app/componentes/mi-dash-board/mi-dash-board.component.ts
+++ salaDeJuegos/src/app/componentes/mi-dash-board/mi-dash-board.component.ts
@@ -13,10 +13,10 @@ export class MiDashBoardComponent {
     map(({ matches }) => {
       if (matches) {
         return [
-          { title: 'Card 1', cols: 1, rows: 1 },
-          { title: 'Card 2', cols: 1, rows: 1 },
-          { title: 'Card 3', cols: 1, rows: 1 },
-          { title: 'Card 4', cols: 1, rows: 1 }
+          { title: 'RPS', cols: 1, rows: 1, img: "ppt.png", texto: 'Elija su arma y juegue contra la computadora', ruta: '/rps' },
+          { title: 'Aritmetica', cols: 1, rows: 1, img: "aritmetica.jpg", texto: 'Pruebe la destreza de su mente', ruta: '/Aritmetica' },
+          { title: 'Adivine el numero', cols: 1, rows: 1, img: "adivina.jpg", texto: 'Pruebe su suerte', ruta: '/AdivinaNumero' },
+          { title: 'Adivine el pais', cols: 1, rows: 1, img: "banderas.jpg", texto:'Demuestre su conocimiento en geografia', ruta: '/Banderas'  }
         ];
       }
 
